b26e40a9c60975812873402aacb317a170b71a83
Jamal El Youssefi <jamal.elyoussefi@yahoo.fr>
2020-11-21 16:07:36 +0100

    Fixing geolocalisation issue

--- app.py
+++ app.py
@@ -5,44 +5,50 @@ from pymysql import escape_string as thwart
 from datetime import datetime
 import json
 import requests
-import demjson
+from threading import Condition
 
+class Covid19Monitor():
+    def __init__(self):
+        self.cv = Condition()
+        self.app = Flask(__name__)
+        self.app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+        self.position = {'latitude': 0, 'longitude': 0}
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    def start(self):
+        app = self.app
+        @app.route("/", methods=['GET', 'POST'])
+        def register():
+            form = RegistrationForm()
+            if request.method == "POST" and form.validate():
+                date_enregistrement =  datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                prenom = form.prenom.data
+                nom = form.nom.data
+                date_de_naissance = form.date_de_naissance.data.strftime('%Y-%m-%d %H:%M:%S')
+                lieu_de_naissance = form.lieu_de_naissance.data
+                cin = form.cin.data
+                print("\n Current Position {}\n".format(self.position))
+                c,conn = connection()
+                c.execute("INSERT INTO  register (prenom, nom, date_de_naissance, lieu_de_naissance, cin, date_enregistrement, position) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                        (thwart(prenom), thwart(nom), thwart(date_de_naissance), thwart(lieu_de_naissance), thwart(cin), thwart(date_enregistrement), thwart(position)))
 
+                conn.commit()
+                flash("Cas enregistre")
+                c.close()
+                conn.close()
+                return ('', 204)
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html')
+            return render_template('register.html', title='Register', form=form)
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
+        @app.route('/position', methods = ['POST'])
+        def position():
+            if request.method == 'POST':
+                self.position = request.get_json()
 
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-    if request.method == "POST" and form.validate():
-        date_enregistrement =  datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        prenom = form.prenom.data
-        nom = form.nom.data
-        date_de_naissance = form.date_de_naissance.data.strftime('%Y-%m-%d %H:%M:%S')
-        lieu_de_naissance = form.lieu_de_naissance.data
-        cin = form.cin.data
-        position = json.dumps(request.get_json())
-        print(position)
-        c,conn = connection()
-        c.execute("INSERT INTO  register (prenom, nom, date_de_naissance, lieu_de_naissance, cin, date_enregistrement, position) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                (thwart(prenom), thwart(nom), thwart(date_de_naissance), thwart(lieu_de_naissance), thwart(cin), thwart(date_enregistrement), thwart(position)))
-
-        conn.commit()
-        flash("Cas enregistre")
-        c.close()
-        conn.close()
-        return redirect(url_for('home'))
-        
-    return render_template('register.html', title='Register', form=form)
+        self.app.run(host='0.0.0.0', threaded=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    covid19Monitor = Covid19Monitor()
+    covid19Monitor.start()
 
