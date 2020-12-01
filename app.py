b5175dcc67d447c7cecafd815808696bc31f41fe
halima <soulb159@gmail.com>
2020-12-01 22:14:05 +0100

    deploy

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 #import sqlalchemy as db
 
 from flask_googlemaps import GoogleMaps, Map, icons
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
 class Covid19Monitor(object):
@@ -19,10 +21,9 @@ class Covid19Monitor(object):
         self.app = Flask(__name__)
         self.app.config['GOOGLEMAPS_KEY'] = "AIzaSyDcA0xJAaREE2vCdgjDnE-j9HQDChCvmWg"
         GoogleMaps(self.app)
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tygqsltanlysiq:68be0239d03e66b403a43f493822bb0d7b9b776be3d8c0399066436f7d77c6dd@ec2-3-210-23-22.compute-1.amazonaws.com:5432/d8rn5mpu5ua96b'
-        self.db = SQLAlchemy(self.app)
-        self.db.create_all()
-        self.db.session.commit()
+        #self.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tygqsltanlysiq:68be0239d03e66b403a43f493822bb0d7b9b776be3d8c0399066436f7d77c6dd@ec2-3-210-23-22.compute-1.amazonaws.com:5432/d8rn5mpu5ua96b'
+        #self.db = SQLAlchemy(self.app)
+        #self.db.create_all()
         self.app.config['DEBUG'] = True
         self.app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
         self.position = {'latitude': 0, 'longitude': 0}
@@ -61,10 +62,13 @@ class Covid19Monitor(object):
                 #metadata = db.MetaData()
                 #connection = engine.connect()
                 #emp = db.Table('emp', metadata, autoload=True, autoload_with=engine)
-                self.db.create_all()
+                engine = create_engine('postgres://tygqsltanlysiq:68be0239d03e66b403a43f493822bb0d7b9b776be3d8c0399066436f7d77c6dd@ec2-3-210-23-22.compute-1.amazonaws.com:5432/d8rn5mpu5ua96b')
+                Session = sessionmaker(bind=engine)
+                session = Session()
+                #self.db.create_all()
                 cases = Cas(prenom, nom, date, lieu_de_naissance, cin, date_enregistrement, position)
-                self.db.session.add(cases)
-                self.db.session.commit()
+                session.add(cases)
+                session.commit()
                 #c,conn = connection()
                 #c.execute("INSERT INTO  register (prenom, nom, date_de_naissance, lieu_de_naissance, cin, date_enregistrement, position) VALUES (%s, %s, %s, %s, %s, %s, '"+position+"')",
                 #        (thwart(prenom), thwart (nom), thwart(date_de_naissance), thwart(lieu_de_naissance), thwart(cin), thwart(date_enregistrement)))
